ec9622c81a356a12d0e9800a65fcadec86a355be
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-12-21 14:26:23 +0900

    Solve problem

---
+++ Problem_10172.kt
@@ -0,0 +1,14 @@
+/*
+* 백준 10172번. 개
+* https://www.acmicpc.net/problem/10172
+*/
+
+fun main() {
+
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+
+}
