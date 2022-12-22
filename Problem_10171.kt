67addd465afdd24822131dcd9e9f74fdda4cc604
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-12-22 16:22:43 +0900

    Solve problem

---
+++ Problem_10171.kt
@@ -0,0 +1,13 @@
+/*
+* 백준 10171번. 고양이
+* https://www.acmicpc.net/problem/10171
+*/
+
+fun main() {
+
+    println("\\    /\\ ")
+    println(" )  ( ') ")
+    println("(  /  ) ")
+    println(" \\(__)| ")
+
+}
