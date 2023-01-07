7d8003f387de5dd3cc575b8a72c5e2a263351694
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2023-01-08 08:35:27 +0900

    Solve problem

---
+++ 2. Silver/Problem_11279.kt
@@ -0,0 +1,54 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+/*
+* 백준 11279번. 최대 힙
+* https://www.acmicpc.net/problem/11279
+*/
+
+fun main() {
+
+    val arr = initVariable()
+    val result = getResult(arr)
+    printResult(result)
+}
+
+fun initVariable(): ArrayList<Int> {
+
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+
+    val arr = arrayListOf<Int>()
+    repeat(n) {
+        arr.add(br.readLine().toInt())
+    }
+
+    return arr
+}
+
+fun getResult(arr: ArrayList<Int>): ArrayList<Int> {
+
+    val result = arrayListOf<Int>()
+    val queue = PriorityQueue<Int>(Collections.reverseOrder())
+
+    arr.forEach {
+        if (it == 0) {
+            if(queue.isEmpty()) result.add(0)
+            else result.add(queue.remove())
+        } else {
+            queue.add(it)
+        }
+    }
+
+    return result
+}
+
+fun printResult(result: ArrayList<Int>) {
+
+    val bw = System.out.bufferedWriter()
+    result.forEach {
+        bw.write("$it\n")
+    }
+    bw.flush()
+    bw.close()
+}
