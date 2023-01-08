7bdc03fafc4469d785bca403f07a5915638fbff5
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2023-01-08 10:20:51 +0900

    Solve problem

---
+++ 2. Silver/Problem_11727.kt
@@ -0,0 +1,40 @@
+/*
+* 백준 11727번. 2×n 타일링 2
+* https://www.acmicpc.net/problem/11727
+*/
+
+const val max = 1000
+
+fun main() {
+
+    val n = initVariable()
+    val result = getResult(n)
+    printResult(result)
+}
+
+fun initVariable(): Int {
+
+    val br = System.`in`.bufferedReader()
+    return br.readLine().toInt()
+}
+
+fun getResult(n: Int): Int {
+
+    val dp = IntArray(size = max + 1) { 0 }
+    dp[1] = 1
+    dp[2] = 3
+
+    for (i in 3..n) {
+        dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007
+    }
+
+    return dp[n]
+}
+
+fun printResult(result: Int) {
+
+    val bw = System.out.bufferedWriter()
+    bw.write("$result\n")
+    bw.flush()
+    bw.close()
+}
