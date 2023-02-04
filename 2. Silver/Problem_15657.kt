d71f4f802ba6a9be88bc0a41467c2f141fbeac68
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2023-02-05 01:26:13 +0900

    Solve problem

---
+++ 2. Silver/Problem_15657.kt
@@ -0,0 +1,56 @@
+import kotlin.text.StringBuilder
+
+/*
+* 백준 15657번. N과 M (8)
+* https://www.acmicpc.net/problem/15657
+*/
+
+private lateinit var arr: List<Int>
+private val selected = ArrayDeque<Int>()
+private var length = -1
+private val sb = StringBuilder()
+
+private fun main() {
+    initVariable()
+    getResult(0, 0)
+    printResult()
+}
+
+private fun initVariable() {
+    val br = System.`in`.bufferedReader()
+    val (n, m) = br.readLine().split(' ').map { it.toInt() }
+
+    arr = br.readLine().split(' ').map { it.toInt() }
+    arr = arr.sorted()
+
+    length = m
+
+    br.close()
+}
+
+private fun getResult(index: Int, count: Int) {
+
+    if (count == length) {
+        selected.forEach {
+            sb.append("$it ")
+        }
+
+        sb.append("\n")
+        return
+    }
+
+    if (index == arr.size) return
+
+    selected.add(arr[index])
+    getResult(index, count + 1)
+
+    selected.removeLast()
+    getResult(index + 1, count)
+}
+
+private fun printResult() {
+    val bw = System.out.bufferedWriter()
+    bw.write("$sb")
+    bw.flush()
+    bw.close()
+}
