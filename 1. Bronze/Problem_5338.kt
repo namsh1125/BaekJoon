7b5bb6a83a829c925a18ab8f6983f73e041c88f5
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2023-02-27 20:57:08 +0900

    Solve problem

---
+++ 1. Bronze/Problem_5338.kt
@@ -0,0 +1,12 @@
+/*
+* 백준 5338번. 마이크로소프트 로고
+* https://www.acmicpc.net/problem/5338
+*/
+
+private fun main() {
+    println("       _.-;;-._")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-;;-._|")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-''-._|")
+}
