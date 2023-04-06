3323e039266e4993d04f2808dacdce3ed4b7ce63
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2023-04-06 21:31:15 +0900

    Solve problem

---
+++ 1. Bronze/Problem_5524.kt
@@ -0,0 +1,13 @@
+/*
+* 백준 5524번. 입실 관리
+* https://www.acmicpc.net/problem/5524
+*/
+
+private fun main() {
+    val n = readln().toInt()
+
+    repeat(n) {
+        val name = readln()
+        println(name.lowercase())
+    }
+}
